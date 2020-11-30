40121c91a1e46fa2f33f621694bb2c5c06b97609
группы стационара <st@itstep.lan>
2020-11-30 20:48:18 +0300

    30.11.2020

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+let project_folder = "dist";
+let source_folder = "src";
+
+let path={
+    // Выгрузка готовых файлов
+    build:{
+        html:"project_folder" + "/",
+        css:"project_folder" + "/css/",
+        js:"project_folder" + "/js/",
+        img:"project_folder" + "/img/",
+        fonts:"project_folder" + "/fonts/",
+    },
+    // Загрузка файлов для их обработки gulp'ом
+    src:{
+        html:"source_folder" + "/*.html",
+        css:"source_folder" + "/scss/.style.scss",
+        js:"source_folder" + "/js/script.js",
+        img:"source_folder" + "/img/**/*.{png,jpg,gif,webp,svg,ico}",
+        fonts:"source_folder" + "/fonts/*.tff",
+    },
+    // Чтение файлов 
+    watch:{
+        html:"source_folder" + "/**/*.html",
+        css:"source_folder" + "/scss/**/*.scss",
+        js:"source_folder" + "/js//**/*.js",
+        img:"source_folder" + "/img/**/*.{png,jpg,gif,webp.svg,ico}",
+    },
+
+}
+// Подключение плагинов и тд
+    let{src, dest} = require('gulp'),
+    gulp = require('gulp'),
+    browsersync = require("browser-sync").create(),
+    sass = require('gulp-sass');
+
+// Функция обновления страницы
+function browserSync(params){
+    browsersync.init({
+        server:{
+            baseDir:"./"
+        },
+        port:3000,
+        notify:false
+    })
+}
+// Функция для работы с html файлами
+function html(){
+    return src(path.src.html)
+        .pipe(dest(path.build.html))
+        .pipe(browsersync.stream())
+}
+
+gulp.task('sass', function(){
+    gulp.src('./src/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./src'));
+  });
+  
+  gulp.task('sass:watch', function(){
+    gulp.watch('./src/**/*.scss', ['sass']);
+  });
+
+let build = gulp.series(html);
+let watch = gulp.parallel(build,browserSync);
+
+
+// "Дружба файлов"
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
