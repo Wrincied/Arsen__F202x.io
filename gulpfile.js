53ed7e3d8ef3dff81bad60178a6213960ed2ec68
HAINZ <63810038+Wrincied@users.noreply.github.com>
2020-11-30 15:13:13 +0300

    30.11.2020

--- gulpfile.js
+++ gulpfile.js
@@ -4,114 +4,61 @@ let project_folder = "dist";
 let source_folder = "#src";
 
 let path={
+    // Выгрузка готовых файлов
     build:{
-        html:project_folder + "/",
-        css:project_folder + "/css/",
-        js:project_folder + "/js/",
-        img:project_folder + "/img/",
-        fonts:project_folder + "/fonts/",
-},
-    src:{   
-        html:[source_folder + "/*.html","!" + source_folder + "/_*.html"],
-        css:source_folder + "/scss/style.scss",
-        js:source_folder + "/js/script.js",
-        img:source_folder + "/img/**/*.{jpg,png,svg,ico,webp,gif}",
-        fonts:source_folder + "/fonts/*.tt f",
+        html:"project_folder" + "/",
+        css:"project_folder" + "/css/",
+        js:"project_folder" + "/js/",
+        img:"project_folder" + "/img/",
+        fonts:"project_folder" + "/fonts/",
     },
-    watch:{   
-        html:source_folder + "/**/*.html",
-        css:source_folder + "/scss/**/*.scss",
-        js:source_folder + "/js/**/*.js",
-        img:source_folder + "/img/**/*.{jpg,png,svg,ico,webp,gif}",
-    }, 
-    clean:"./" + project_folder + "/"
+    // Загрузка файлов для их обработки gulp'ом
+    src:{
+        html:"source_folder" + "/*.html",
+        css:"source_folder" + "/scss/.style.scss",
+        js:"source_folder" + "/js/script.js",
+        img:"source_folder" + "/img/**/*.{png,jpg,gif,webp,svg,ico}",
+        fonts:"source_folder" + "/fonts/*.tff",
+    },
+    // Чтение файлов 
+    watch:{
+        html:"source_folder" + "/**/*.html",
+        css:"source_folder" + "/scss/**/*.scss",
+        js:"source_folder" + "/js//**/*.js",
+        img:"source_folder" + "/img/**/*.{png,jpg,gif,webp.svg,ico}",
+    },
+
 }
-let{src,dest} = require('gulp'),
+// Подключение плагинов и тд
+    let{src, dest} = require('gulp'),
     gulp = require('gulp'),
-    browsersync = require("browser-sync").create(),
-    fileinclude = require("gulp-file-include"),
-    del = require("del"),
-    scss = require("gulp-sass"),
-    autoprefixer= require("gulp-autoprefixer"),
-    group_media = require("gulp-group-css-media-queries"),
-    rename = require("gulp-rename");
+    browsersync = require("browser-sync").create();
 
-
-
-
-    function browserSync(params) {
-        browsersync.init({
-            server:{
-                baseDir:"./" + project_folder + "/"
-            },
-            port:8848,
-            notify:false
-        })
-    }
-
-
-
-
-    function html() {
-        return src(path.src.html)
-            .pipe(fileinclude())
-            .pipe(dest(path.build.html))
-            .pipe(browsersync.stream())
-    }
-
-    function js() {
-        return src(path.src.js)
-            .pipe(fileinclude())
-            .pipe(dest(path.build.js))
-            .pipe(browsersync.stream())
-    }
-
-
-    function css() {
-        
-        return src(path.src.css)
-        .pipe(
-            scss({outputStyle:"compressed"})
-        )
-        .pipe(
-            autoprefixer({
-                overrideBrowserlist: ["last 5 version"],
-                cascade: true
-            })
-        )
-        .pipe(
-            group_media()
-        )
-
-        .pipe(
-            rename({
-                extname:".min.css"
-            })
-        )
-
-        .pipe(dest(path.build.css))
+// Функция обновления страницы
+function browserSync(params){
+    browsersync.init({
+        server:{
+            baseDir:"./" + project_folder + "/"
+        },
+        port:3000,
+        notify:false
+    })
+}
+// Функция для работы с html файлами
+function html(){
+    return src(path.src.html)
+        .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
-    }
+}
 
 
-    function watchFiles(params) {
-        gulp.watch([path.watch.html], html);
-        gulp.watch([path.watch.css], css);
-        gulp.watch([path.watch.js], js);
-    }
 
-    function clean(params) {
-        return del(path.clean);
-    }
+let build = gulp.series(html);
+let watch = gulp.parallel(build,browserSync);
 
 
-    let build = gulp.series(clean,gulp.parallel(js,css,html));
-    let watch =gulp.parallel(build,watchFiles,browserSync); 
-
-    exports.js = js;
-    exports.css = css;
-    exports.html = html;
-    exports.build = build;
-    exports.watch = watch;
-    exports.default = watch;
-    
+// "Дружба файлов"
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
