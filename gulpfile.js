716cf6facb6d1292f0be6962e9db9a06e073fbf6
HAINZ <63810038+Wrincied@users.noreply.github.com>
2020-12-07 22:30:30 +0300

    Add project

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,53 @@
-let project_folder = "dist";
-let source_folder = "src";
+let gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync= require ('browser-sync'),
+uglify = require('gulp-uglify'),
+concat = require('gulp-concat'),
+rename = require('gulp-rename');
 
-let path={
-    // Выгрузка готовых файлов
-    build:{
-        html:"project_folder" + "/",
-        css:"project_folder" + "/css/",
-        js:"project_folder" + "/js/",
-        img:"project_folder" + "/img/",
-        fonts:"project_folder" + "/fonts/",
-    },
-    // Загрузка файлов для их обработки gulp'ом
-    src:{
-        html:"source_folder" + "/*.html",
-        css:"source_folder" + "/scss/.style.scss",
-        js:"source_folder" + "/js/script.js",
-        img:"source_folder" + "/img/**/*.{png,jpg,gif,webp,svg,ico}",
-        fonts:"source_folder" + "/fonts/*.tff",
-    },
-    // Чтение файлов 
-    watch:{
-        html:"source_folder" + "/**/*.html",
-        css:"source_folder" + "/scss/**/*.scss",
-        js:"source_folder" + "/js//**/*.js",
-        img:"source_folder" + "/img/**/*.{png,jpg,gif,webp.svg,ico}",
-    },
+gulp.task('scss' ,function(){
+    return gulp.src('src/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({stream:true}))
+});
 
-}
-// Подключение плагинов и тд
-    let{src, dest} = require('gulp'),
-    gulp = require('gulp'),
-    browsersync = require("browser-sync").create(),
-    sass = require('gulp-sass');
+gulp.task('html',function(){
+    return gulp.src('src/*.html')
+    .pipe(browserSync.reload({stream:true}))
+});
 
-// Функция обновления страницы
-function browserSync(params){
-    browsersync.init({
-        server:{
-            baseDir:"./"
-        },
-        port:3000,
-        notify:false
-    })
-}
-// Функция для работы с html файлами
-function html(){
-    return src(path.src.html)
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
-}
-
-gulp.task('sass', function(){
-    gulp.src('./src/**/*.scss')
-      .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('./src'));
-  });
-  
-  gulp.task('sass:watch', function(){
-    gulp.watch('./src/**/*.scss', ['sass']);
-  });
-
-let build = gulp.series(html);
-let watch = gulp.parallel(build,browserSync);
+gulp.task('script',function(){
+    return gulp.src('src/*.html')
+    .pipe(browserSync.reload({stream:true}))
+});
 
 
-// "Дружба файлов"
-exports.html = html;
-exports.build = build;
-exports.watch = watch;
-exports.default = watch;
+gulp.task('js',function(){
+    return gulp.src([
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+});
+
+
+gulp.task('watch' ,function(){
+    gulp.watch('src/scss/**/*.scss',gulp.parallel('scss'))
+    gulp.watch('src/*.html',gulp.parallel('html'))
+    gulp.watch('src/js/*.js',gulp.parallel('script'))
+});
+
+gulp.task('default',gulp.parallel('browser-sync', 'watch'))
